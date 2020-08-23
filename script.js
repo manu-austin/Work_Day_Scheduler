fada394be6bca5a0534ff097a44e48d784734b18
Emmanuel Durand <shernua@gmail.com>
2020-08-22 23:23:34 -0500

    last minute modifications

--- script.js
+++ script.js
@@ -6,17 +6,121 @@ var actualDate = moment().format('DDMMYYYY');
 
 // put value to dates and extract dates on the value on each key
 var nineAmContent = localStorage.getItem("9AM");
-var nineAmContentClean = nineAmContent.substring(8, nineAmContent.length);
-var nineAmTimeStored = nineAmContent.substring(0, 8);
-
+var tenAmContent = localStorage.getItem("10AM");
+var elevenAmContent = localStorage.getItem("11AM");
+var twelvePmContent = localStorage.getItem("12PM");
+var onePmContent = localStorage.getItem("1PM");
+var twoPmContent = localStorage.getItem("2PM");
+var threePmContent = localStorage.getItem("3PM");
+var fourPmContent = localStorage.getItem("4PM");
+var fivePmContent = localStorage.getItem("5PM");
 
 
 $(document).ready(function() {
 
-    if (actualDate === nineAmTimeStored) {
-        $("#9amContent").val(nineAmContentClean);
-    } else {
-        localStorage.removeItem("9AM");
+
+    if (nineAmContent != null) { // verify this is not empty otherwise substring generates error
+        var nineAmContentClean = nineAmContent.substring(8, nineAmContent.length);
+        var nineAmTimeStored = nineAmContent.substring(0, 8); // get date info was stored
+
+        if (actualDate === nineAmTimeStored) { // compare date it was stored and today's date, if it is today => keep the data, otherwise clean
+            $("#9amContent").val(nineAmContentClean);
+        } else {
+            localStorage.removeItem("9AM");
+        }
+    }
+
+
+    if (tenAmContent != null) {
+        var tenAmContentClean = tenAmContent.substring(8, tenAmContent.length);
+        var tenAmTimeStored = tenAmContent.substring(0, 8);
+        if (actualDate === tenAmTimeStored) {
+            $("#10amContent").val(tenAmContentClean);
+        } else {
+            localStorage.removeItem("10AM");
+        }
+    }
+
+    if (elevenAmContent != null) {
+        var elevenAmContentClean = elevenAmContent.substring(8, elevenAmContent.length);
+        var elevenAmTimeStored = elevenAmContent.substring(0, 8);
+        if (actualDate === elevenAmTimeStored) {
+            $("#11amContent").val(elevenAmContentClean);
+        } else {
+            localStorage.removeItem("11AM");
+        }
+    }
+
+    if (twelvePmContent != null) {
+        var twelvePmContentClean = twelvePmContent.substring(8, twelvePmContent.length);
+        var twelvePmTimeStored = twelvePmContent.substring(0, 8);
+        if (actualDate === twelvePmTimeStored) {
+            $("#12pmContent").val(twelvePmContentClean);
+        } else {
+            localStorage.removeItem("12PM");
+        }
+    }
+
+    if (onePmContent != null) {
+        var onePmContentClean = onePmContent.substring(8, onePmContent.length);
+        var onePmTimeStored = onePmContent.substring(0, 8);
+        if (actualDate === onePmTimeStored) {
+            $("#1pmContent").val(onePmContentClean);
+        } else {
+            localStorage.removeItem("1PM");
+        }
+
+    }
+
+
+    if (twoPmContent != null) {
+
+        var twoPmContentClean = nineAmContent.substring(8, twoPmContent.length);
+        var twoPmTimeStored = nineAmContent.substring(0, 8);
+        if (actualDate === twoPmTimeStored) {
+            $("#2pmContent").val(twoPmContentClean);
+        } else {
+            localStorage.removeItem("2PM");
+        }
+
+    }
+
+    if (threePmContent != null) {
+
+        var threePmContentClean = threePmContent.substring(8, threePmContent.length);
+        var threePmTimeStored = threePmContent.substring(0, 8);
+        if (actualDate === threePmTimeStored) {
+            $("#3pmContent").val(threePmContentClean);
+        } else {
+            localStorage.removeItem("3PM");
+        }
+
+    }
+
+    if (fourPmContent != null) {
+
+        var fourPmContentClean = fourPmContent.substring(8, fourPmContent.length);
+        var fourPmTimeStored = fourPmContent.substring(0, 8);
+        if (actualDate === fourPmTimeStored) {
+            $("#4pmContent").val(fourPmContentClean);
+        } else {
+            localStorage.removeItem("4PM");
+        }
+
+    }
+
+
+    if (fivePmContent != null) {
+
+        var fivePmContentClean = fivePmContent.substring(8, fivePmContent.length);
+        var fivePmTimeStored = fivePmContent.substring(0, 8);
+
+        if (actualDate === fivePmTimeStored) {
+            $("#5pmContent").val(fivePmContentClean);
+        } else {
+            localStorage.removeItem("5PM");
+        }
+
     }
 
 
